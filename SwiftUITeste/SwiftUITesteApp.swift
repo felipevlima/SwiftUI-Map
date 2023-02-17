34e3306c0b46fa5a152c6b0b00a3f59bdcc163f0
Felipe Vieira <felipe.lima@bancointer.com.br>
2023-02-17 14:07:47 -0300

    Initial Commit

---
+++ SwiftUITeste/SwiftUITesteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITesteApp.swift
+//  SwiftUITeste
+//
+//  Created by Felipe Vieira Lima on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITesteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
